b1b98f518abcf6a2cdd8c8841a8d0d7e6d7883eb
Navnit Tiwari <42030960+navnittiwari@users.noreply.github.com>
2020-06-06 17:07:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,53 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", function(req, res) {
-  var today=new Date();
+app.get("/", function (req, res) {
+  var today = new Date();
   var currentDay = today.getDay();
-  
-  if(currentDay === 6 || currentDay === 0){
-    res.send("great its a weekend");
+
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "sunday";
+      break;
+
+    case 1:
+      day = "monday";
+      break;
+
+    case 2:
+      day = "tuesday";
+      break;
+
+    case 3:
+      day = "wednesday";
+      break;
+
+    case 4:
+      day = "thursday";
+      break;
+
+    case 5:
+      day = "friday";
+      break
+
+      case 6:
+        day="saturday";
+        break;
+
+    default:
+      console.log("error! current day is: "+currentDay);
   }
-  else{
-    res.sendFile(__dirname+"/index.html");
-  }
+
+  res.render("list", {
+    kindOfDay: day
+  });
 
 });
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("server started on port 3000");
 })
