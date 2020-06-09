32d1bcd4c19f182738a3a4c02d7fa8488d7e616d
Navnit Tiwari <42030960+navnittiwari@users.noreply.github.com>
2020-06-09 13:43:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["buy food", "cook food", "eat food"];
+let items = ["buy food", "cook food", "eat food"];
 
 app.set('view engine', 'ejs');
 
@@ -11,22 +11,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
   
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 })
